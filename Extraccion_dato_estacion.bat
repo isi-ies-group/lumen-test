d3acfa82b008101ec2d895aaec15167f00c27974
davidrb_ <david.rodriguez.blanco@alumnos.upm.es>
2023-05-30 13:09:45 +0200

    Estación Tiempo Real

---
+++ Extraccion_dato_estacion.bat
@@ -0,0 +1,2 @@
+C:\Users\David\miniconda3\python.exe  C:\Users\David\Documents\GitHub\lumen-test\Extraccion_dato_estacion.py
+C:\Users\David\Documents\GitHub\lumen-test\lumen serve Prueba_EstacionTiempoReal --show
